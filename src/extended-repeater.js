b089c5ce7ecc5791bed09ef2a3b52cae78b4907a
treyer <nemereno_by@mail.ru>
2021-10-01 17:28:46 +0300

    feat: implement carbon-dating, extended-reapeater and hanoi-tower functionality

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+export default function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let additionString = '';
+  if (options.hasOwnProperty('addition')){
+    let addition = String(options.addition);
+    let additionSeparator = (options.hasOwnProperty("additionSeparator")) ? options.additionSeparator : "|";
+    let additionRepeatTimes = (options.hasOwnProperty('additionRepeatTimes')) ? options.additionRepeatTimes : 1;
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      if (additionRepeatTimes === 1){
+        additionString = addition;
+      } else {
+        (i < options.additionRepeatTimes - 1) ? additionString += addition + additionSeparator : additionString += addition;
+      }
+    }
+  }
+
+  let res = '';
+  let repeatTimes = (options.hasOwnProperty('repeatTimes')) ? options.repeatTimes : 1;
+  let separator = (options.hasOwnProperty('separator')) ? options.separator : "+";
+  str = String(str);
+  for (let i = 0; i < repeatTimes; i++) {
+    (i < options.repeatTimes - 1) ? res += str + additionString + separator : res += str + additionString;
+  }
+
+  return res;
 }
